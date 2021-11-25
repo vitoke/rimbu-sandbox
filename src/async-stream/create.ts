ebc207090500d5e8020fd7741bda434940d0f340
Arvid Nicolaas <arvid.nicolaas@philips.com>
2021-11-25 12:10:41 +0100

    initial commit

---
+++ src/async-stream/create.ts
@@ -0,0 +1,35 @@
+import { AsyncStream, Stream } from "@rimbu/core";
+import { log, subject } from "../utils/log";
+
+const streamFromValues = AsyncStream.of(
+  1,
+  () => 3,
+  Promise.resolve(2),
+  async () => 4
+);
+const streamFromIterable = AsyncStream.from(
+  "word"
+);
+const emptyStream = AsyncStream.empty<number>();
+const rangeStream = AsyncStream.from(
+  Stream.range({
+    start: 3,
+    amount: 4
+  })
+);
+
+async function doAsync() {
+  subject("Create AsyncStream");
+  log({
+    streamFromValues,
+    streamFromValuesToArray: await streamFromValues.toArray(),
+    streamFromIterable,
+    streamFromIterableToArray: await streamFromIterable.toArray(),
+    emptyStream,
+    emptyStreamToArray: await emptyStream.toArray(),
+    rangeStream,
+    rangeStreamToArray: await rangeStream.toArray()
+  });
+}
+
+doAsync();
---
+++ src/async-stream/query.ts
@@ -0,0 +1,25 @@
+import { AsyncStream, Stream } from "@rimbu/core";
+import { log, subject } from "../utils/log";
+
+subject("Query AsyncStream");
+
+const values = Stream.randomInt(0, 100).take(20);
+// prevent every iteration from giving new values
+const stream = AsyncStream.from(values.toArray());
+
+async function doAsync() {
+  const contains5 = await stream.contains(5);
+  const allOver8 = await stream.every(
+    (v) => v > 8
+  );
+  const minValue = await stream.min();
+
+  log({
+    streamToArray: await stream.toArray(),
+    contains5,
+    allOver8,
+    minValue
+  });
+}
+
+doAsync();
